6a7ef493f6afa658a2cbfa8b35263cd9175e877f
danielplatt <d.platt@web.de>
2019-07-16 11:13:52 +0100

    Included comments

--- Puzzle.py
+++ Puzzle.py
@@ -8,6 +8,9 @@ start_time = time.time()
 persons = 4
 number_for_access = 3
 
+# Puzzle:
+# Can one lock a box with multiple locks and distribute keys to the boxes (potentially more than one key per box) to a number of persons, such that the box can be unlocked [number_for_access] persons, but cannot be unlocked by [number_for_access]-1 persons?
+
 locks = 1
 
 while True:
--- Unlockattempt.py
+++ Unlockattempt.py
@@ -1,25 +1,27 @@
 import itertools
 
 class Unlockattempt (object):
+    """Given a number of locks an distribution of keys this class has methods to determine whether it is a solution to the puzzle."""
     def __init__(self,locks,persons,number_for_access):
         self.locks              = locks
         self.persons            = persons
         self.number_for_access  = number_for_access
 
     def check_unlockable_combination(self):
-        unlockable_by_number_for_access = True
+        """Check if the given distribution is a solution to the puzzle."""
+
         for i in itertools.combinations(range(self.persons), self.number_for_access):
             if not self.check_unlockable(i):
-                unlockable_by_number_for_access=False
                 return False
-        unlockable_by_number_for_access_minus_one = False
+
         for i in itertools.combinations(range(self.persons), self.number_for_access-1):
             if self.check_unlockable(i):
-                unlockable_by_number_for_access_minus_one = True
                 return False
+
         return True
 
     def check_unlockable(self,persons_chosen):
+        """Given the key distribution and a subset of persons, determine whether they can unlock the box."""
         unlockable = True
         for i in self.locks:
             if set(i).isdisjoint(persons_chosen):
